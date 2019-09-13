c22e5fd128d11b80730ecfc14e875693177c8b0b
greyels <greyels@gmail.com>
2019-09-13 15:11:05 +0300

    Initial commit

---
+++ input.py
@@ -0,0 +1,7 @@
+URLS = ('https://cleantalk.org/blacklists/submited_today',
+        'https://cleantalk.org/blacklists/updated_today',
+	    'https://cleantalk.org/blacklists/spam-ip')
+
+DOMAINS = ('example.com',)
+
+OUTPUT_FILE = './cleantalk.xlsx'
