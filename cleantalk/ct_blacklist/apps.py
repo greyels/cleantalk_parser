826d58a4f772a6915df7b077d9b7496881a5a691
greyels <greyels@gmail.com>
2019-10-10 16:16:45 +0300

    django initial commit

---
+++ cleantalk/ct_blacklist/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CtBlacklistConfig(AppConfig):
+    name = 'ct_blacklist'
--- input.py
+++
@@ -1,7 +0,0 @@
-URLS = ('https://cleantalk.org/blacklists/submited_today',
-        'https://cleantalk.org/blacklists/updated_today',
-	    'https://cleantalk.org/blacklists/spam-ip')
-
-DOMAINS = ('example.com',)
-
-OUTPUT_FILE = './cleantalk.xlsx'
